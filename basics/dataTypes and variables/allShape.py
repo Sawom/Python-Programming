12693cd5d1d51795c453e509a0b126fa22274424
Sawom <64780532+Sawom@users.noreply.github.com>
2021-05-13 17:40:39 +0600

    Add files via upload

---
+++ basics/dataTypes and variables/allShape.py
@@ -0,0 +1,34 @@
+print("input for Triangle: ")
+base = float(input("Enter base: "))
+height = float(input("Enter height: "))
+tResult = .5*base*height
+print(" area of triangle: ",tResult)
+
+print("\ninput for Rectangle: ")
+w = float(input("Enter width: "))
+l = float(input("Enter length: "))
+rResult = w *l
+print(" area of Rectangle: ",rResult)
+
+print("\ninput for Trapezium: ")
+h = float(input("Enter height: "))
+l = float(input("Enter length: "))
+w = float(input("Enter width: "))
+trResult = .5*(h+l)*h
+print(" area of Rectangle: ",trResult)
+
+print("\ninput for Ellipse: ")
+a = float(input("Enter a: ") )
+b = float(input("Enter b: "))
+eResult = 3.14*a*b
+print(" area of Rectangle: ",eResult)
+
+print("\ninput for Square: ")
+a = int( input("Enter a: ") )
+sResult = a*a
+print(" area of Square: ",sResult)
+
+print("\ninput for Circle: ")
+r = float(input("Enter radious: ") )
+sResult = 3.14*r*r
+print(" area of Square: ",sResult)
---
+++ basics/dataTypes and variables/variable.py
@@ -0,0 +1,5 @@
+name = input("Enter name: ")
+age= input("Enter age: \n")
+cgpa = input("Enter cgpa: \n")
+
+print("output shown below.....\n"+"name : "+name+"\nage: "+age+"\ncgpa: "+cgpa)

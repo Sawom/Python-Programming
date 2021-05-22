85d04460cce9db37176dd671ffb6ff70a78a2445
Abdur Rashid Sawom <asawom250@gmail.com>
2021-05-23 02:55:09 +0600

    Delete Conditional Logic directory

--- Conditional Logic/saarc.py
+++
@@ -1,7 +0,0 @@
-saarc = ["Bangladesh","SriLanka","Nepal","Bhutan","Afghanistan","Maldives","Pakistan","India"]
-country = input("Enter a name of country: ")
-
-if country in saarc:
-    print(country,"is a member of saarc")
-else :
-    print(country,"is not a member of SAARC")

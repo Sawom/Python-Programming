ba90b63c005daac16cda8dfdbb2836c548fa48fc
Abdur Rashid Sawom <asawom250@gmail.com>
2021-05-22 22:11:13 +0600

    Add files via upload

---
+++ Conditional Logic/saarc.py
@@ -0,0 +1,7 @@
+saarc = ["Bangladesh","SriLanka","Nepal","Bhutan","Afghanistan","Maldives","Pakistan","India"]
+country = input("Enter a name of country: ")
+
+if country in saarc:
+    print(country,"is a member of saarc")
+else :
+    print(country,"is not a member of SAARC")

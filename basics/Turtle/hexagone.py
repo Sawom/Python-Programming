73a61572c34dda1d080dffeb229889e3186f2480
Abdur Rashid Sawom <asawom250@gmail.com>
2021-05-28 12:20:16 +0600

    Add files via upload

---
+++ basics/Turtle/hexagone.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.shape('turtle')
+turtle.speed(1)
+
+turtle.forward(200)
+turtle.left(60)
+turtle.forward(200)
+turtle.left(60)
+turtle.forward(200)
+turtle.left(60)
+turtle.forward(200)
+turtle.left(60)
+turtle.forward(200)
+turtle.left(60)
+turtle.forward(200)
+
+turtle.exitonclick()

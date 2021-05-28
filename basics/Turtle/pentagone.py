7213c08c5dddb66f56ce4b305c7e44e88ab5f106
Abdur Rashid Sawom <asawom250@gmail.com>
2021-05-28 12:20:41 +0600

    Add files via upload

---
+++ basics/Turtle/pentagone.py
@@ -0,0 +1,15 @@
+import turtle
+turtle.shape('turtle')
+turtle.speed(1)
+
+turtle.forward(112)
+turtle.left(72)
+turtle.forward(200)
+turtle.left(54)
+turtle.forward(200)
+turtle.left(108)
+turtle.forward(200)
+turtle.left(54)
+turtle.forward(200)
+
+turtle.exitonclick()

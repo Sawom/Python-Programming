ba5d782dc0e315f85d32c2d2a2d53de87ad1947f
Abdur Rashid Sawom <asawom250@gmail.com>
2021-06-21 04:22:20 +0600

    Add files via upload

---
+++ basics/tuple set dictionary/tuple.py
@@ -0,0 +1,10 @@
+student = (
+    ("abdur rashid",19101008,'A'),
+    ("rashid",64564),
+    "jannat",'mou'
+)
+print(student[1])
+print(student[2:])
+print(student)
+li = list(student)
+print("list = ",li)

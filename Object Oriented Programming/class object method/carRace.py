8b7b45b2f23df0017c174e9dad8fd97bbd487c1a
Abdur Rashid Sawom <asawom250@gmail.com>
2021-06-21 04:11:47 +0600

    Add files via upload

---
+++ Object Oriented Programming/class object method/carRace.py
@@ -0,0 +1,11 @@
+class car:
+    name = "premio"
+    color = 'white'
+
+    def start():
+        print("starting the engine")
+
+print("name of the car: ",car.name)
+print("color:",car.color)
+
+car.start()

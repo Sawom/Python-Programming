d90109ace1ad33ffb85042d3b678a60002a80bb8
Abdur Rashid Sawom <asawom250@gmail.com>
2021-06-21 04:19:16 +0600

    Add files via upload

---
+++ basics/tuple set dictionary/dictionary.py
@@ -0,0 +1,11 @@
+students = {
+    101 : 'jannat',
+    102 : 'rashid',
+    103 : 'rodry',
+    104 : 'maliha',
+    105 : 'nasim'
+}
+
+print(students)
+print(students[101])
+print(students.get(109,"not in dictionary"))

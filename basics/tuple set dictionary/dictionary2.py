c7478bbb20bea068e0fc11aa06be3748fb7c537a
Abdur Rashid Sawom <asawom250@gmail.com>
2021-06-21 04:20:28 +0600

    Add files via upload

---
+++ basics/tuple set dictionary/dictionary2.py
@@ -0,0 +1,9 @@
+marks = {
+    101 : {'bangla': 75, 'english': 79, "math":82},
+    102 : {'bangla': 72, 'english': 78, "math":80},
+    103 : {'bangla': 77, 'english': 80, "math":71},
+    104 : {'bangla': 69, 'english': 96, "math":99}
+}
+
+print(marks)
+print(marks[102]["math"])

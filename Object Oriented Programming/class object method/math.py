f671eb6dff0b837921199ca29ea294a7c5b19dcb
Abdur Rashid Sawom <asawom250@gmail.com>
2021-06-21 04:14:48 +0600

    Add files via upload

---
+++ Object Oriented Programming/class object method/math.py
@@ -0,0 +1,12 @@
+import math
+
+num1 = int(input("enter 1st number : "))
+num2 = int(input("enter 2nd number : "))
+
+print("PI = ",math.pi)
+print("Power = ",math.pow(num1,num2))
+print("square root of num1 =  ",math.sqrt(num1))
+print("Floor of an example = ",math.floor(5.325))
+print("Ceil of an example = ",math.ceil(5.325))
+print("gcd of num1 and num2 = ",math.gcd(num1,num2))
+print("lcm of num1 and num2 = ",math.lcm(num1,num2,45))

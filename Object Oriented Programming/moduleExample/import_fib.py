6f2624ef94e893a704734ebfa5247ed8c00f10e3
Abdur Rashid Sawom <asawom250@gmail.com>
2021-06-21 04:05:58 +0600

    Add files via upload

---
+++ Object Oriented Programming/moduleExample/import_fib.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    import fibonacci
+    n= int(input("enter number"))
+    n = fibonacci.find_fibonacci(n)
+    print("result = ",n)
+
+'''
+if __name__ == '__main__':
+    import trial
+    print("hello! I'm from fibonacci.py !")
+    print(trial.__name__)
+'''

f877620bcb346bf9846515b425c82757d1957277
Abdur Rashid Sawom <asawom250@gmail.com>
2021-06-21 04:11:17 +0600

    Add files via upload

---
+++ Object Oriented Programming/class object method/car2.py
@@ -0,0 +1,16 @@
+# example of constructor
+class car:
+    name = ""
+    color = ""
+
+    def __init__(self,name,color):
+        self.name = name
+        self.color = color
+
+    def start(self):
+        print("Starting the engine")
+
+my_car = car("corolla","white")
+print(my_car.name)
+print(my_car.color)
+my_car.start()

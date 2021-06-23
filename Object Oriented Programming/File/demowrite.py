d6c4dbffced06cdc7c5a1115cc4f7979cb4e5652
Abdur Rashid Sawom <asawom250@gmail.com>
2021-06-24 02:55:05 +0600

    Add files via upload

---
+++ Object Oriented Programming/File/demowrite.py
@@ -0,0 +1,5 @@
+file = open("writeDemo.txt","a")
+text = file.write("Taylor Alison Swift (born December 13, 1989) is an American singer-songwriter.\n")
+
+text = file.write("Her narrative lyricism, which often takes inspiration from her personal life and experiences, has received widespread critical praise and media coverage.")
+file.close()
